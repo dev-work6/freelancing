d5ca02111a3d7a5d9f645f3084238bb013272413
devdaim6 <daimdev6@gmail.com>
2024-12-01 18:59:28 +0530

    Aurora BG add

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { AuroraBackground } from "@/components/ui/aurora-background";
+
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <AuroraBackground>
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </AuroraBackground>
   );
 }
