5009d15ae61eddf4030fa0002b5136de54eee2f5
devdaim6 <daimdev6@gmail.com>
2024-12-07 15:54:27 +0530

    Wavy Background add for mobile devices

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -9,7 +9,6 @@ import {
 } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-
 export const HeroParallax = ({
   products,
 }: {
@@ -146,6 +145,7 @@ export const ProductCard = ({
 
 export const Header = () => {
   return (
+              
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
       <div className="space-y-6">
         <h1 className="text-2xl md:text-7xl font-bold bg-gradient-to-r from-primary via-primary/80 to-primary/60 bg-clip-text text-transparent">
@@ -170,9 +170,9 @@ export const Header = () => {
               AI Integration
             </span>
           </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
  
